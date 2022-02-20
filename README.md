2e13d0c9cf5b5f16046f3176d0f78432b753cbdc
0xR3y <0xR3y@protonmail.com>
2022-02-20 15:37:14 +0300

    update

--- README.md
+++ README.md
@@ -1,29 +1,50 @@
-## Rust Pulsar Network
+## Pulsar Network
 
-A connection Library for the Pulsar Network, a distributed hash table peer-to-peer messaging protocol written in Rust.
-
-### Usage
-
-In your `Cargo.toml`:
-
-```
-
-[dependencies]
-pulsar-network = "0.1.2"
-
-```
-
-In your rust file:
-
-```
-
-use pulsar_network::{ Config, Network, Message, MessageKind };
-
-```
+Pulsar Network is a distributed hash table peer-to-peer messaging protocol for the Astreuos Blockchain written in Rust.
 
 ### Features
+- Send & Receive Messages between Peers.
+- A Message contains the nonce, body, 
+- Message encryption using chacha20poly1305 and a blake3 hash, of the shared point on Curve 25519, as the key.
+- Peers can be pinged and respond with their public key & routes.
+- Currently supported route is Astreuos Blockchain Validation.
+ 
 ### API
+
+`Connect`
+
+```
+use pulsar_network::Network;
+
+let network = Network::connect();
+
+network.validation = true; // join the validation route
+
+```
+
+`Message`
+
+```
+
+use pulsar_network::Message;
+
+let msg: &str = "Hello";
+
+let mut message = Message::new(msg);
+
+message.expiry = 7_u8; // the default expiry is 1
+
+```
+
+`Broadcast`
+
+```
+
+network.validation.broadcast(message);
+
+```
+
 ### Contributions
 Pull requests, bug reports and any kind of suggestion are welcome.
 
-2020-12-09
+2022-02-20
--- src/lib.rs
+++ src/lib.rs
@@ -1,140 +1,29 @@
+use fides::asymmetric;
 
-use neutrondb::Store;
-// use stellar_notation::{ encode, decode };
-use opis::Int;
-
-use std::sync::mpsc;
-use std::net::UdpSocket;
-use std::error::Error;
-use std::thread;
-
-#[derive(Debug, Clone)]
-pub enum MessageKind {
-    Block,
-    Transaction,
-    Join,
-    Get,
-    Text
-}
-
-#[derive(Debug, Clone)]
-pub struct Message {
-    pub kind: MessageKind,
-    pub body: Vec<u8>,
-    pub signature: Vec<u8>
-}
-
-#[derive(Debug, Clone)]
-pub struct Route { id: Int, ip: String }
-
-#[derive(Debug, Clone)]
-pub struct Bucket { distance: Int, routes: Vec<Route> }
-
-#[derive(Debug, Clone)]
-pub struct Config { id: String, store: Store }
+#[derive(Debug)]
+pub struct Route {}
 
 #[derive(Debug)]
 pub struct Network {
-    pub id: Int,
-    pub sender: mpsc::Sender<Message>,
-    pub receiver: mpsc::Receiver<Message>,
-    pub buckets: Vec<Bucket>,
-    pub store: Store
+    private_key: [u8;32],
+    public_key: [u8;32],
+    pub validation: Route 
 }
 
 impl Network {
-    
-    pub fn configure(config: Config) -> Result<Network, Box<dyn Error>> {
 
-        let (sender, receiver) = mpsc::channel();
+    pub fn connect() -> Network {
 
-        let id = Int::from_str(&config.id, 16)?;
+        let priv_key = asymmetric::private_key();
 
-        let mut network = Network {
-            id: id,
-            sender: sender,
-            receiver: receiver,
-            buckets: Vec::new(),
-            store: config.store
-        };
-
-        let routes = network.store.get_all()?;
-
-        let mut routes_vec: Vec<Route> = Vec::new();
-
-        match routes {
-
-            Some(res) => {
-                
-                routes_vec = res.iter()
-                    .map(|x| {
-
-                        let id: Int = Int::from_str(&x.0, 16).unwrap();
-                        let ip: String = x.1.to_string();
-                        
-                        Route {
-                            id: id,
-                            ip: ip
-                        }
-
-                    })
-                    .collect();
-
-            },
-
-            None => ()
-
+        let pub_key = asymmetric::public_key(&priv_key);
+        
+        Network {
+            private_key: priv_key,
+            public_key: pub_key,
+            validation: Route {},
         }
 
-        network.buckets = (1..256)
-            .map(|x| {
-
-                let distance: Int = Int::from_str(&x.to_string(), 10).unwrap();
-
-                let mut bucket_routes = routes_vec.clone();
-
-                bucket_routes.retain(|x| network.id.clone().sub(&x.id).is_equal(&distance));
-
-                Bucket {
-                    distance: distance,
-                    routes: bucket_routes
-                }
-
-            })
-            .collect();
-
-        Ok(network)
-
     }
-
-    pub fn connect(self) {
-
-        thread::spawn(move || {
-
-            let network: Network = self;
-            
-            loop {
-
-                let socket = UdpSocket::bind("127.0.0.1:7577").unwrap();
-        
-                let mut buf = [0; 10];
-        
-                let (_amt, _src) = socket.recv_from(&mut buf).unwrap();
-
-                let message = Message {
-                    kind: MessageKind::Block,
-                    body: vec![],
-                    signature: vec![]
-                };
-        
-                network.sender.send(message).unwrap();
-        
-            }
-            
-        });
-
-    }
-
-    pub fn broadcast() {}
-
+    
 }
